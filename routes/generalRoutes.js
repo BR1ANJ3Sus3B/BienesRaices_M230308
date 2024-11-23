45b651b6a99b700294a5bca33b23b0e4b8749434
Brian J <230308@utxicotepec.edu.mx>
2024-11-22 21:26:31 -0600

    Brian-Practica11
    Agregación de las carpetas layout, public, y tailwind

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -1,18 +1,21 @@
 import express from 'express'
+
 const router = express.Router()
 
 
 
-router.get ("/",function(req,res){
+router.get("/", function(req, res){
     res.send("Hola desde la web en NodeJS")
 })
-router.get("/quienEres",function(req,res){
-    res.json({
-        "NOmbre":"Brian jesus mendoza marquez",
-        "Carrera":"TI DSM ",
-        "Grado":"4",
-        "Grupo":"B"
 
+router.get("/quienEres", function(req, res){
+    res.json({
+        "nombre" : "Brian jesus mendoza marquez",
+        "carrera" : "TI DSM",
+        "grado" : "4",
+        "grupo" : "B",
+        "Signo zodiacal" : "Aries"
     })
 })
+
 export default router;
