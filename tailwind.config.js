5b965ef8fdba31cea433661935010e79d3e555d0
Brian J <230308@utxicotepec.edu.mx>
2024-11-24 10:43:17 -0600

    Examen-Practico
    Agregacion de animacion en los errores modificacion en la bienvenida

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customLightBlue: '#B6C3F5',       // Azul claro personalizado
-        customButtonColor: '#FFFFFF',    // Naranja vibrante
-        customLightGreen: '#74F4E8',     // Verde agua claro
-        skyBlue: '#87CEEB',              // Azul cielo
-        palePurple: '#000000',           // Turquesa
-        vibrantYellow: '#A1CDF4',        // Amarillo vibrante
-        deepPurple: '#673AB7',           // Morado profundo
-        softPink: '#000022',             // Rosa suave
-        forestGreen: '#228B22',          // Verde bosque
+        customLightBlue: '#B6C3F5',
+        customButtonColor: '#FFFFFF',
+        customLightGreen: '#74F4E8',
+        skyBlue: '#87CEEB',
+        palePurple: '#000000',
+        vibrantYellow: '#A1CDF4',
+        deepPurple: '#673AB7',
+        softPink: '#000022',
+        forestGreen: '#228B22',
+      },
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out', // Animación fade-in
+        'fade-out': 'fadeOut 1s ease-in forwards', // Animación fade-out para el ocultamiento
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
       },
     },
   },
